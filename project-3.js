fd914ad3a986a6a7f9b5f5e0075aceccc021f161
wanting <wantingpeng.cici@gmail>
2024-06-13 21:37:43 +0200

    add linstener on container
    add new class activ, then change the stylesheet add .active .class to override the style

--- project-3.js
+++ project-3.js
@@ -1,7 +1,18 @@
-const question=Array.from(document.querySelectorAll('.question'))
+
+/* version -----------------------1 */
+/* const question=Array.from(document.querySelectorAll('.question'))
 const answer=Array.from(document.querySelectorAll('.answer'))
 console.log(question)
 
 question.forEach((e,index)=>e.addEventListener('click',function(){
   answer[index].style.height=answer[index].style.height==='0px'?'150px':'0px'
+}))
+ */
+/* version -----------------------2 */
+
+const content=Array.from(document.querySelectorAll('.content'))
+
+content.forEach((el)=>el.addEventListener('click',function(){
+el.classList.toggle('active')
+
 }))
