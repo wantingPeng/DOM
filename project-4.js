0856e230f9700db27f4dcd4acc3bff5b52b10650
wanting <wantingpeng.cici@gmail>
2024-06-14 23:10:40 +0200

    fineshed the frame of <div id="task-container">
    when click button'+'>>create  div id task> <button id check-box>check box and  <button> bin
    at end first get element task-container,, then append all in it

--- project-4.js
+++ project-4.js
@@ -1,5 +1,6 @@
 // 1 每一秒钟 计数一次second count ++ ,然后60秒 变成minute ++ 然后hours++
 //监听start stop button ,点击start icon切换成stop,并且开始计时 点击stop icon 切换成start 并且停止计时
+// reset: listen click  tome display =00:00:00
 let displayTimer=document.querySelector("#timer");
 let stopStartBtn=document.querySelector('#startStopBtn')
 
@@ -19,6 +20,8 @@ second<10?showSecond='0'+second.toString():showSecond=second;
 displayTimer.innerText=showHours+':'+showMinute+':'+showSecond;
 }
 let playid=document.querySelector('#play')
+let reset=document.querySelector('#reset')
+
 //The event object has a target property which contains information about the element 
 //that actually received the event.
 stopStartBtn.addEventListener('click',function(e){
@@ -35,3 +38,7 @@ else{
 }
 
 })
+reset.addEventListener('click',function(){
+  displayTimer.innerText="00:00:00";
+})
+
---
+++ project-5.js
@@ -0,0 +1,33 @@
+
+//add event listener at <button id="add-task"> 
+//when click>>create  <div id task> <button id check-box>check box and  <button> bin at end first get element task-container,, then append all in it
+//mean while <div class task> take value form Enter Task
+
+//add event listener at 
+
+const addTask=document.querySelector('#add-task')
+const taskContainer=document.querySelector('#task-container')
+const inputTask=document.querySelector('#input-task')
+/* addTask.addEventListener('click', function(){
+const tastContainer=document.createElement('div')
+tastContainer.classList.add('tastContainer'); */
+
+addTask.addEventListener('click', function(){
+  const task=document.createElement('div')
+  task.innerText=`${inputTask.value}`
+  task.setAttribute('id','task')
+  taskContainer.append(task)
+
+  const checkBox=document.createElement('button')
+  checkBox.setAttribute('id','check-box')
+  checkBox.innerHTML='<i class="fa-solid fa-check"></i>';
+  console.log(checkBox.id)
+  taskContainer.append(checkBox)
+  console.log(checkBox)
+
+  const bin=document.createElement('button')
+  bin.innerHTML='<i class="fa-solid fa-trash-can"></i>';
+  bin.setAttribute('id',"bin" )
+  taskContainer.append(bin)
+
+})
